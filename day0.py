1af22d7759269b69bdc847d5322d778f0dafe1d9
RagePly <the.hugo.lom@gmail.com>
2024-11-18 18:26:46 +0100

    Prep: Remove test file from history

--- day0.py
+++
@@ -1,8 +0,0 @@
-def part1(src):
-    return sum(map(int, src.splitlines()))
-
-from functools import reduce
-from operator import mul
-
-def part2(src):
-    return reduce(mul, map(int, src.splitlines()))
